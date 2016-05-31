7e48e99eee43c32fa7ff552cacb02285147c2ceb
LifeBot <onecodeman@protonmail.com>
2016-06-01 00:14:19 +0100

    First Commit :)

---
+++ README.md
@@ -0,0 +1,14 @@
+PTBot
+===
+A [Discord](http://discord.gg) bot written in Node using [Discord.js](https://github.com/hydrabolt/discord.js).
+
+Getting the bot
+---
+Invite the bot to your server using this [link](https://discordapp.com/oauth2/authorize?&client_id=184422435163078656&scope=bot).
+
+Run locally
+---
+
+ - Clone this repo
+ - Run `npm install` to get the required dependencies
+ - Run `node app.js`
---
+++ app.js
@@ -0,0 +1,66 @@
+var Discord = require("discord.js");
+var request = require("superagent");
+var botToken = "MTg0NDMwODYxMjA1MjQxODU4.CiUUwg.g7ez9ly0twKuEJ2f31mIcH1tu5k";
+var SOUNDDIR = "sounds/"
+var bot = new Discord.Client();
+
+//when the bot is ready
+bot.on("ready", function () {
+	console.log("Bot in: " + bot.channels.length + " channels");
+});
+
+//when the bot disconnects
+bot.on("disconnected", function () {
+	//alert the console
+	console.log("Disconnected!");
+
+	//exit node.js with an error
+	process.exit(1);
+});
+
+//when the bot receives a message
+bot.on("message", function (msg) {
+
+	// Sounds
+	if(msg.content.startsWith("!som")){
+		var rest = msg.content.split(" ");
+		rest.splice(0,1);
+		rest=rest.join(" ");
+		var sound = SOUNDDIR+rest+".mp3"
+		bot.joinVoiceChannel(msg.author.voiceChannel.id, function (err, con){
+			if (err){ return console.log(err); }
+			con.playFile(sound, function(err, intent){
+				intent.on("end", function(){
+					con.destroy();
+				});
+			});
+		});
+	}
+
+	//List voice channels
+	if (msg.content == "%listVoice"){
+		for (var channel of msg.channel.server.channels) {
+			if (channel instanceof Discord.VoiceChannel) {
+				bot.reply(msg, channel.name + " - " + channel.id);
+			}
+		}
+	}
+
+	//Join voice channel
+	if (msg.content.startsWith("&voice")) {
+		var rest = msg.content.split(" ");
+		rest.splice(0,1);
+		rest=rest.join(" ");
+		//bot.reply(msg, "Trying to join: " + channel.name + " " + channel.id);
+		bot.joinVoiceChannel(rest).catch(error);
+	}
+
+});
+
+// This function is used by &init to handle connection errors
+function error(e) {
+	console.log(e.stack);
+	process.exit(0);
+}
+
+bot.loginWithToken(botToken);
